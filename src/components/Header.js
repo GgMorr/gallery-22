aa76056e5d4e00d4132b7da861d55d018042c623
Batya <Gigi.ucla21@gmail.com>
2022-09-17 17:22:47 -0700

    create components

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Nav from './Nav';
+
+
+
+const Header = () => {
+    return (
+        <>
+            <header id="intro">
+                <article className="fullheight">
+                    <div className="hgroup">
+                        <h1>The Humphrey Hotel</h1>
+                        <h2>West Side</h2>
+                        <p><a href="#welcome"><img src="https://landonhotel.com/images/misc/arrow.png" alt="down arrow" /></a></p>
+                    </div>
+                </article>
+                <Nav />
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import menuLinksData from './data/menu_links.json';
+
+const Nav = () => {
+    return (
+
+            <nav id="nav">
+                <div className="navbar">
+                    <div className="brand"><a href="#welcome">Our <span> Hotel</span></a></div>
+                    <ul>
+                        {
+                            menuLinksData.map((link) =>
+                            <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
+                       )
+                     }
+
+                    </ul>
+                </div>
+            </nav>
+    
+    )
+}
+
+export default Nav;
